9c0a46d548b63b9c3529005eb9ad49bc9a050bac
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-01-20 15:18:16 +0100

    feat: overview

--- container/Overview/Overview.jsx
+++ container/Overview/Overview.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { OverviewContainer } from "./Overview.style";
 import { CryptoCurrencyMarket } from "react-ts-tradingview-widgets";
+import { Header } from "@/components/Header";
+
+import dynamic from "next/dynamic";
+const SymbolOverviewNoSSR = dynamic(
+  () => import("react-ts-tradingview-widgets").then((w) => w.SymbolOverview),
+  {
+    ssr: false,
+  }
+);
 
 const Overview = () => {
   return (
     <OverviewContainer>
-      {/* Overview */}
+      Overview
+      {/* <Header/> */}
       <CryptoCurrencyMarket
         colorTheme="dark"
         width="100%"
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['react-ts-tradingview-widgets'],
 }
 
 module.exports = nextConfig
